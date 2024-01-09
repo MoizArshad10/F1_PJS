4577dbb4b5a4c17360953b87ee9611ec67927a54
Moiz <moiz@gmail.com>
2024-01-09 18:19:04 +0500

    save

---
+++ first.js
@@ -0,0 +1,45 @@
+console.log("WELCOME TO PJS");
+console.log("HELLO WORLD");
+
+// DATA TYPES 
+
+firstName = "Mike";   //STRING
+age = 15;             //INT , NUMBER
+x = null;              //NULL means empty
+y = undefined;         // value assign hogii in future 
+isCnic = false;        // boolean
+
+console.log(typeof(isCnic));
+console.log(typeof(firstName));
+console.log(typeof(age));
+console.table(firstName , age , x  , y , isCnic);
+
+// Two Types of data types
+// PRIMITIVE STRING , INT , NUMBER , BOOLEAN etc
+// NON PRIMITIVE ARRAY , OBJECT , FUNCTION etc
+
+// THREE TYPES OF DECLERATION
+
+// var can be redeclare and updated
+// let can't be redeclared but it can be updated
+// const cant' be redeclared or can't be updated
+
+var city = "KARACHI ";
+console.log(city);
+city = "Islamabad";
+console.log(city);
+var city = "Lahore";
+console.log(city);
+
+
+let myAge = 15;
+console.log(myAge);
+myAge = 18;
+console.log(myAge);
+
+const pie = 3.14;
+console.log(pie);
+pie = 4.25;
+
+
+
